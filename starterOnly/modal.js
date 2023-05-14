72046c6ecf725d230d670ac0c5d0ba0bb9ddabc5
Laurent Bigey <laurent.bigey@outlook.fr>
2023-05-14 09:24:15 +0200

    phrase de validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,100 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const modalClose = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 const submitButton = document.querySelector(".btn-submit");
+const form = document.querySelector("form[name='reserve']");
+const dataInput = document.querySelectorAll(".formData > input");
+const locationChecked = document.querySelectorAll("input[name='location']:checked");
+
+//input listeners
+dataInput.forEach((input) => input.addEventListener("input", validate));
+
+//validation
+function validate() {
+  //définit le type d'input pour savoir quel test passer
+  const type = this.getAttribute("type");
+  //regexp pour l'email
+  const emailRegex = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
+  //AFFICHAGE DE LA PHRASE DE VALIDATION DES INPUTS
+  //on supprime d'abord la phrase existante :
+  //1-définir l'ID de la phrase
+  const inputID = this.getAttribute("id");
+  const validatorID = inputID+"-validator";
+  //2-la sélectionner :
+  const exValidator = document.getElementById(validatorID);
+  //3-la supprimer si elle existe
+  if (exValidator != null) {
+    document.getElementById(validatorID).remove();
+  }
+  //on crée ensuite la nouvelle :
+  let p = document.createElement('p');
+  this.parentNode.appendChild(p);
+  //on lui ajoute une classe et un id
+  const newValidator = document.querySelector(".formData > p");
+  newValidator.classList.add("validator");
+  newValidator.setAttribute("id",validatorID);
+  switch (type) {
+    case "text":
+      if(this.value.length >= 2) {
+        document.getElementById(validatorID).remove();
+      }
+      else if(this.value.length === 0) {
+        p.innerHTML = "Ce champ est obligatoire";
+      }
+      else {
+        p.innerHTML = "Veuillez entrer au moins 2 caractères";
+      }
+      break;
+      
+    case "email":
+      if(emailRegex.test(this.value)) {
+        document.getElementById(validatorID).remove();
+      }
+      else if(this.value.length === 0) {
+        p.innerHTML = "Ce champ est obligatoire";
+      }
+      else {
+        p.innerHTML = "Veuillez entrer une adresse e-mail valide";
+      }
+      break;
+      
+    case "number":
+      console.log(this.value.length);
+      if(isNaN(this.value)) {
+        p.innerHTML = "Veuillez entrer une valeur numérique";
+      }
+      else if(this.value.length === 0) {
+        p.innerHTML = "Ce champ est obligatoire";
+      }
+      else {
+        document.getElementById(validatorID).remove();
+      }
+      break;
+      
+    case "radio":
+      if(locationChecked.length === 1) {
+        document.getElementById(validatorID).remove();
+      }
+      else {
+        p.innerHTML = "Veuillez choisir une option";
+      }
+      break;
+      
+    case "checkbox":
+      if(this.hasAttribute('required') && this.checked === true) {
+        document.getElementById(validatorID).remove();
+      }
+      else if(this.hasAttribute('required') && this.checked === false) {
+        p.innerHTML = "Veuillez accepter les termes et conditions";
+      }
+      else {
+        document.getElementById(validatorID).remove();
+      }
+      break;
+  
+    default:
+      break;
+  }
+}
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -32,8 +126,10 @@ function closeModal() {
 
 /**JE BLOQUE L'ENVOI DU FORMULAIRE POUR POUVOIR LANCER LA VALIDATION */
 
+/*
 // prevent submit event
 submitButton.addEventListener("click", preventSubmit);
+
 
 // prevent submit function
 function preventSubmit(event) {
@@ -55,4 +151,9 @@ function validate() {
   console.log(quantity);
   console.log(location);
   /**IF (CONDITIONS OK) {ENVOYER LE FORMULAIRE VIA JS} */
-}
+  /*if (firstname === "azerty")
+  {
+    form.submit();
+    console.log('submit');
+  }*/
+//}
